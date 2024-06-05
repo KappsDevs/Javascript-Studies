659c7a25aa77a76efd70888eefdcbe59cfe92d1d
Stefanni Kapps <stefannikapps29@gmail.com>
2024-06-05 19:57:23 -0300

    Section 2- Course

--- JavaScript/basics-01-starting-project/assets/scripts/app.js
+++ JavaScript/basics-01-starting-project/assets/scripts/app.js
@@ -2,17 +2,68 @@
 // `(${currentResult} + 10)` 
 
 const DEFAULT = 0
-
-
 let currentResult = DEFAULT;
+let logEntries = [];
 
-
-
-
-
-function showSum(){
-    let result = currentResult + userInput.value;
-    outputResult(result, " ");
+function getUserNumberInput(){
+    return parseInt(userInput.value);
 }
 
-addBtn.addEventListener("click", showSum);
+function createAndWriteOutput(operator, resutlBeforeCalc, calcNumber){
+    const calcDescription = `${resutlBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function writeLog(operationIdentifier, prevResult, operationNumber,newResult){
+    
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    };
+
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+
+function add(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput("+",initialResult, enteredNumber);
+    
+    writeLog("ADD", initialResult, enteredNumber, currentResult);
+}
+
+function sub(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput("-", initialResult, enteredNumber);
+    writeLog("SUB", initialResult, enteredNumber, currentResult);
+
+}
+
+function mul(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput("*", initialResult, enteredNumber);
+    writeLog("MUL", initialResult, enteredNumber, currentResult);
+
+}
+
+function div(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput("/", initialResult, enteredNumber);
+    writeLog("DIV", initialResult, enteredNumber, currentResult);
+
+}
+
+addBtn.addEventListener("click", add);  
+subtractBtn.addEventListener("click", sub);
+multiplyBtn.addEventListener("click", mul);
+divideBtn.addEventListener("click", div);
